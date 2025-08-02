334656615f6f8293f0cba0e17ae5ddf7cd45e152
Xrainer101 <146404942+Xrainer101@users.noreply.github.com>
2025-08-02 00:37:58 -0700

    Made base components of game
    Made 2D platforming, platforms, moving platforms, and a modular interaction system. I also added a coin, but idk if I'll use that.

---
+++ Scripts/test_interactable.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	interaction_area.interact = Callable(self, "_on_interact")
+
+
+func _on_interact():
+	print("Interaction test successful")
